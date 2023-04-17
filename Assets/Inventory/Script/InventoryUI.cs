0a28db51096255c94f2b521c8de60ce39ae1ffc6
jjuuuunnii <jake991110@naver.com>
2023-04-18 00:17:29 +0900

    Add inventory dissappear

--- Assets/Inventory/Script/InventoryUI.cs
+++ Assets/Inventory/Script/InventoryUI.cs
@@ -1,4 +1,55 @@
-using System.Collections;
+using UnityEngine;
+
+public class InventoryUI : MonoBehaviour
+{
+    public CanvasGroup inventoryCanvasGroup;
+    public float fadeTime = 0.5f;
+
+    private bool isShowing = false;
+    private float currentFadeTime = 0f;
+
+    private void Start()
+    {
+        // 시작할 때는 InventoryUI를 숨긴다.
+        inventoryCanvasGroup.alpha = 0f;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            isShowing = !isShowing;
+            currentFadeTime = 0f;
+        }
+
+        if (isShowing && currentFadeTime < fadeTime)
+        {
+            float t = currentFadeTime / fadeTime;
+            inventoryCanvasGroup.alpha = Mathf.Lerp(0f, 1f, t);
+            currentFadeTime += Time.deltaTime;
+        }
+        else if (!isShowing && currentFadeTime < fadeTime)
+        {
+            float t = currentFadeTime / fadeTime;
+            inventoryCanvasGroup.alpha = Mathf.Lerp(1f, 0f, t);
+            currentFadeTime += Time.deltaTime;
+        }
+        else if (!isShowing)
+        {
+            inventoryCanvasGroup.alpha = 0f;
+        }
+    }
+}
+
+
+
+//탭누르면 등장장
+        /*if(Input.GetKeyDown(KeyCode.Tab)){
+            Debug.Log("Inventory on");
+            activeInventory = !activeInventory;
+            inventoryPanel.SetActive(activeInventory);
+        }*/
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -18,4 +69,6 @@ public class InventoryUI : MonoBehaviour
             inventoryPanel.SetActive(activeInventory);
         }
     }
-}
+}*/
+
+
